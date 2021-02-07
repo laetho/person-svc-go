8252e3201cd7d9c485bf2b125ef11ce9bf10464e
laetho <thorsplass@gmail.com>
2021-02-07 11:29:30 +0100

    Added a migration for creating the persons table

--- persons.go
+++ persons.go
@@ -1,5 +1,12 @@
 package main
 
+const PersonMigration = `CREATE TABLE IF NOT EXISTS person (
+id serial primary key,
+name varchar(150) unique not null,
+phone int not null,
+address varchar(150) not null
+);`
+
 type Person struct {
 	Id      int32  `json:"id"`
 	Name    string `json:"name"`
