90f28c60ce6fe7a6974bfccceda03b1a4bff0bd3
laetho <thorsplass@gmail.com>
2021-02-07 11:30:21 +0100

    Added code to run the persons migration

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"strconv"
 )
 
-// Concurrency safe Postgres conenction pool. Going global here for such
+// Concurrency safe Postgres connection pool. Going global here for such
 // a simple application.
 var dbcp *pgxpool.Pool
 var dsn PostgresDSN
@@ -19,29 +19,43 @@ var dsn PostgresDSN
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
-		os.Exit(-1)
+		log.Printf("Error loading .env file, ignoring...")
 	}
 
 	dsn = NewPostgresDSN()
 	if !dsn.Valid() {
 		log.Fatalf("Unable to Generate Postgresql DSN. Missing environment variables?")
-		os.Exit(-1)
 	}
 }
 
 func main() {
 	var err error
+
 	dbcp, err = pgxpool.Connect(context.Background(), dsn.ToString())
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer dbcp.Close()
+	
+
+	// Run migration
+	conn , err := dbcp.Acquire(context.Background())
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Unable to get DB connection: %v\n", err)
+	}
+	defer conn.Release()
+
+	_ , err = conn.Query(context.Background(), PersonMigration)
+	if err != nil {
+		panic(err)
+	}
 
 	port := 8080
 	http.HandleFunc("/status", statusHandler)
 	http.HandleFunc("/persons", getPersonsHandler)
 	http.HandleFunc("/create", createPersonHandler)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+
 }
+
